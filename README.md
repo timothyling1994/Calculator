f9146781a5fa419ea46aa1b82ef9fa9b2883dd99
Timothy Ling <timothyling1994@gmail.com>
2020-12-08 16:15:44 -0800

    Add all functionality, edge case handling, changes to styling, and modify readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Calculator
+
+Skills Used:
+
+1. Utilizing Grid and Flex for layout
+2. JS logic for handling all calculator functions
+3. Code organization
--- calc.js
+++ calc.js
@@ -1,21 +1,21 @@
 function add(num1,num2)
 {
-	return num1+num2;
+	return DecimalPrecision2.trunc(num1+num2,9);
 }
 
 function subtract(num1,num2)
 {
-	return num1-num2;
+	return DecimalPrecision2.trunc(num1-num2,9);
 }
 
 function multiply(num1,num2)
 {
-	return num1*num2;
+	return DecimalPrecision2.trunc(num1*num2,9);
 }
 
 function divide(num1,num2)
 {
-	return num1/num2;
+	return DecimalPrecision2.trunc(num1/num2,9);
 }
 
 function power(num1,num2)
@@ -33,7 +33,7 @@ function factorial(num1)
 	return num1 * factorial(num1-1);
 }
 
-function operate(num1,num2,operator)
+function operate(operator,num1,num2)
 {
 	num1 = parseFloat(num1);
 	num2 = parseFloat(num2);
@@ -44,31 +44,37 @@ function operate(num1,num2,operator)
 			display["sequence"]=String(add(num1,num2));
 			updateDisplay();
 			display["operator"]="";
-			display["operatorlimit"]=false;
+			display["isOperatorSet"]=false;
 			break;
 		case "-":
 			display["sequence"]=String(subtract(num1,num2));
 			updateDisplay();
 			display["operator"]="";
-			display["operatorlimit"]=false;
+			display["isOperatorSet"]=false;
 			break;
 		case "*":
 			display["sequence"]=String(multiply(num1,num2));
 			updateDisplay();
 			display["operator"]="";
-			display["operatorlimit"]=false;
+			display["isOperatorSet"]=false;
 			break;
 		case "/":
 			display["sequence"]=String(divide(num1,num2));
 			updateDisplay();
 			display["operator"]="";
-			display["operatorlimit"]=false;
+			display["isOperatorSet"]=false;
+			break;
+		case "!":
+			display["sequence"]=String(factorial(num1));
+			updateDisplay();
+			display["operator"]="";
+			display["isOperatorSet"]=false;
 			break;
 		case "^":
 			display["sequence"]=String(power(num1,num2));
 			updateDisplay();
 			display["operator"]="";
-			display["operatorlimit"]=false;
+			display["isOperatorSet"]=false;
 			break;
 
 	}
@@ -77,11 +83,14 @@ function operate(num1,num2,operator)
 
 let display = {
 	"sequence":"",
-	"operatorlimit":false,
+	"isOperatorSet":false,
 	"operator":"",
 	"operatorList":["+","-","*","/","^","!"],
 	"num1":"",
 	"num2":"",
+	"num1decimalSet":false,
+	"num2decimalSet":false,
+	"digitLimit":false
 };
 
 function updateDisplay()
@@ -93,21 +102,102 @@ function updateDisplay()
 function clearSequence()
 {
 	display["sequence"] = "";
-	updateDisplay();
-	display["operatorlimit"]=false;
+	display["isOperatorSet"]=false;
 	display["operator"]="";
+	display["num1"]="";
+	display["num2"]="";
+	display["num1decimalSet"]=false;
+	display["num2decimalSet"]=false;
+
+	updateDisplay();
+
 }
+
+function changePlusMinus()
+{
+
+	if(display["isOperatorSet"]==false)
+	{
+		if(display["sequence"][0] != '-')
+		{
+			display["sequence"] = '-' + display["sequence"];
+			updateDisplay();
+		}
+		else
+		{
+			display["sequence"] = display["sequence"].substring(1);
+			updateDisplay();
+		}
+	}
+	else
+	{
+
+		let indexOfOperator = display["sequence"].substring(1).indexOf(display["operator"])+1;
+
+		if(display["sequence"][indexOfOperator+1] != '-')
+		{
+		
+			display["sequence"] = display["sequence"].substring(0, indexOfOperator+1) + '-' + display["sequence"].substring(indexOfOperator+1);
+			updateDisplay();
+		}
+		else
+		{
+			display["sequence"] = display["sequence"].substring(0, indexOfOperator+1) + display["sequence"].substring(indexOfOperator+2);
+			updateDisplay();
+		}
+	}	
+}
+
+var DecimalPrecision2 = (function() {
+    if (Number.EPSILON === undefined) {
+        Number.EPSILON = Math.pow(2, -52);
+    }
+    if (Math.trunc === undefined) {
+        Math.trunc = function(v) {
+            return v < 0 ? Math.ceil(v) : Math.floor(v);
+        };
+    }
+    var isRound = function(num, decimalPlaces) {
+        //return decimalPlaces >= 0 &&
+        //    +num.toFixed(decimalPlaces) === num;
+        var p = Math.pow(10, decimalPlaces);
+        return Math.round(num * p) / p === num;
+    };
+    var decimalAdjust = function(type, num, decimalPlaces) {
+        if (isRound(num, decimalPlaces || 0))
+            return num;
+        var p = Math.pow(10, decimalPlaces || 0);
+        var m = (num * p) * (1 + Number.EPSILON);
+        return Math[type](m) / p;
+    };
+    return {
+        // Decimal round (half away from zero)
+        round: function(num, decimalPlaces) {
+            return decimalAdjust('round', num, decimalPlaces);
+        },
+        // Decimal ceil
+        ceil: function(num, decimalPlaces) {
+            return decimalAdjust('ceil', num, decimalPlaces);
+        },
+        // Decimal floor
+        floor: function(num, decimalPlaces) {
+            return decimalAdjust('floor', num, decimalPlaces);
+        },
+        // Decimal trunc
+        trunc: function(num, decimalPlaces) {
+            return decimalAdjust('trunc', num, decimalPlaces);
+        },
+        // Format using fixed-point notation
+        toFixed: function(num, decimalPlaces) {
+            return decimalAdjust('round', num, decimalPlaces).toFixed(decimalPlaces);
+        }
+    };
+})();
 
 function storeNumbers()
 {
-	let classList = this.getAttribute('class');
-	let display_panel = document.querySelector("#display");
 
-	if(classList.includes('operator') && display["operatorlimit"]==true)
-	{
-		display_panel.textContent = "TOO MANY OPERATORS BRUH";
-	}
-	else
+	if(display["operator"]!= "!")
 	{
 		display["sequence"] += (this.getAttribute('value'));
 		updateDisplay();
@@ -115,36 +205,115 @@ function storeNumbers()
 
 }
 
+function setErrorMessage(message)
+{
+	let error_console = document.querySelector("#error-console");
+	error_console.textContent = "Err: " + message;
+}
+
+function clearErrorMessage()
+{
+	let error_console = document.querySelector("#error-console");
+	error_console.textContent = "";
+}
+
 function setOperator()
 {
-	if(display["operatorlimit"] == false)
+
+	let display_panel = document.querySelector("#display");
+
+	if(display["isOperatorSet"] == false)
 	{
-		display["operatorlimit"] = true;
+		display["isOperatorSet"] = true;
 		display["operator"] = this.getAttribute('value');
+		display["sequence"] += (this.getAttribute('value'));
+		updateDisplay();
+	}
+	else
+	{
+		setErrorMessage("Too many operators");
 	}
 }
 
 function evaluateFunction()
 {
+
 	let sequence = display["sequence"];
-	//let indexOfOperator = sequence.indexOf(display["operator"]);
-	//if(indexOfOperator>-1)
-	if(display["operator"] != "")
+	let display_panel = document.querySelector("#display");
+
+	if(display["operator"]=="!")
+	{
+		let indexOfOperator = sequence.indexOf(display["operator"]);
+		display["num1"] = sequence.substr(0,indexOfOperator);
+
+		if(display["num1"].includes("."))
+		{
+			setErrorMessage("Cannot add decimal when evaluating a factorial");
+		}
+		else if (parseFloat(display["num1"]) < 0)
+		{
+			setErrorMessage("Can't do factorial of a negative number");
+		}
+		else
+		{
+			operate(display["operator"],display["num1"]);
+		}
+
+	}
+
+	else if(display["operator"] != "")
 	{
 		let indexOfOperator = sequence.indexOf(display["operator"]);
 		display["num1"] = sequence.substr(0,indexOfOperator);
 		display["num2"] = sequence.substr(indexOfOperator+1);
-		operate(display["num1"],display["num2"] ,display["operator"]);
+		operate(display["operator"],display["num1"],display["num2"]);
+	}
+}
+
+function setDecimal()
+{
+
+	let display_panel = document.querySelector("#display");
+	let error_console = document.querySelector("#error-console");
+
+	if(display["operator"]=="" && display["num1decimalSet"]==false)
+	{
+		if(display["sequence"]=="")
+		{
+			display["sequence"] += "0"+(this.getAttribute('value'));
+		}
+		else
+		{
+			display["sequence"] += (this.getAttribute('value'));	
+		}
+		updateDisplay();
+		display["num1decimalSet"]=true;
+	}
+	else if(display["operator"]=="!")
+	{
+		setErrorMessage("Cannot add decimal when evaluating a factorial");	
+	}
+
+	else if(display["operator"]!= "" && display["num2decimalSet"]==false)
+	{
+		display["sequence"] += (this.getAttribute('value'));
+		updateDisplay();
+		display["num2decimalSet"]=true;
+	}
+	else
+	{
+		setErrorMessage("Too many decimals");	
 	}
 }
 
 function delete_button()
 {
+
 	let strLength = display["sequence"].length;
 
 	if(display["operatorList"].indexOf(display["sequence"][strLength-1])>-1)
 	{
-		display["operatorlimit"] = false;
+		display["isOperatorSet"] = false;
 		display["operator"] = "";
 	}
 
@@ -155,9 +324,15 @@ function delete_button()
 
 function theDomHasLoaded(e) {
 
-	const buttons = document.querySelectorAll('.buttons');
+	const buttons = document.querySelectorAll('.numbers');
 	buttons.forEach(button => {
 		button.addEventListener('click',storeNumbers);
+		
+	});
+
+	const all = document.querySelectorAll('.all');
+	all.forEach(element => {
+		element.addEventListener('click',clearErrorMessage);
 		
 	});
 
@@ -167,6 +342,9 @@ function theDomHasLoaded(e) {
 		
 	});
 
+	const decimal_btn = document.querySelector('#decimal');
+	decimal_btn.addEventListener('click',setDecimal);
+
 	const delete_btn = document.querySelector('#delete');
 	delete_btn.addEventListener('click',delete_button);
 
@@ -175,6 +353,9 @@ function theDomHasLoaded(e) {
 
 	const equal = document.querySelector('#equal');
 	equal.addEventListener('click',evaluateFunction);
+
+	const plusminus = document.querySelector('#plusminus');
+	plusminus.addEventListener('click',changePlusMinus);
 
 }
 
